4628eb3754223af1c9a326de2b1c1895e57056f5
MurabitoF <francomurabito@outlook.com>
2022-07-12 16:26:22 -0300

    Fixing firebase config,  install styled components

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, GithubAuthProvider, signInWithPopup } from 'firebase/auth'
 
-initializeApp({
+const firebaseConfig = {
   apiKey: 'AIzaSyDCSNhC6AYUk11WP1EB8HlUPMukHORg2Cs',
   authDomain: 'devscord-1c632.firebaseapp.com',
   projectId: 'devscord-1c632',
@@ -9,9 +9,11 @@ initializeApp({
   messagingSenderId: '873374326862',
   appId: '1:873374326862:web:c4baf38542504fadbd231f',
   measurementId: 'G-P83D0B27TD'
-})
+}
 
-const auth = getAuth()
+const app = initializeApp(firebaseConfig)
+
+export const auth = getAuth(app)
 
 export const signInWithGithub = () => {
   const provider = new GithubAuthProvider()
